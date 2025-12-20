d0b9c29e20737a478dd0702ad0ab5580bd49f734
Rafael Mori <faelmori@gmail.com>
2025-12-20 11:23:34 +0300

    feat: adiciona suporte a Content-Transfer-Encoding em mensagens RFC e aprimora o envio de emails via Gmail
    Signed-off-by: Rafael Mori <faelmori@gmail.com>

--- tools/mail/provider/gmail.go
+++ tools/mail/provider/gmail.go
@@ -11,26 +11,46 @@ type GmailProvider struct{}
 
 func (g GmailProvider) Send(cfg *types.MailConnection, msg *types.Email) error {
 	auth := smtp.PlainAuth("", cfg.User, cfg.Pass, cfg.Host)
+	addr := formatAddr(cfg.Host, cfg.Port)
 
-	tlsCfg := &tls.Config{
-		ServerName: cfg.Host,
+	// Porta 465 ou use_ssl => TLS direto
+	if cfg.SSL || cfg.Port == 465 {
+		tlsCfg := &tls.Config{ServerName: cfg.Host}
+		conn, err := tls.Dial("tcp", addr, tlsCfg)
+		if err != nil {
+			return err
+		}
+		client, err := smtp.NewClient(conn, cfg.Host)
+		if err != nil {
+			return err
+		}
+		if err := client.Auth(auth); err != nil {
+			return err
+		}
+		defer client.Close()
+		return sendSMTPMessage(client, msg)
 	}
 
-	conn, err := tls.Dial("tcp", formatAddr(cfg.Host, cfg.Port), tlsCfg)
+	// Porta 587 ou use_tls => STARTTLS
+	client, err := smtp.Dial(addr)
 	if err != nil {
 		return err
 	}
+	defer client.Close()
 
-	client, err := smtp.NewClient(conn, cfg.Host)
-	if err != nil {
+	if err := client.Hello(cfg.Host); err != nil {
 		return err
+	}
+	if ok, _ := client.Extension("STARTTLS"); ok {
+		tlsCfg := &tls.Config{ServerName: cfg.Host}
+		if err := client.StartTLS(tlsCfg); err != nil {
+			return err
+		}
 	}
 
 	if err := client.Auth(auth); err != nil {
 		return err
 	}
-
-	defer client.Close()
 
 	return sendSMTPMessage(client, msg)
 }
